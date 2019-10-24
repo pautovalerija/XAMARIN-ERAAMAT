b080fb10fa4c1d966d360a49e05931fce9d1d708
Opilane TTHK <opilane@tthk.ee>
2019-10-24 15:52:47 +0300

    2

---
+++ ERaamatPautova/ERaamatPautova/GlavStr.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ERaamatPautova
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GlavStr : ContentPage
+    {
+        public GlavStr()
+        {
+            InitializeComponent();
+        }
+    }
+}
